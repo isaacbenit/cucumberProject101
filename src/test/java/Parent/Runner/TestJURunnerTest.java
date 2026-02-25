b350e3225f2fd44806c75c4a2160e4b3c68e7430
isaac benit <isaac.irakoze24snhu@keplercollege.ac.rw>
2026-02-25 15:58:21 +0200

    I have added in the login, register and update functionality

--- src/test/java/Parent/Runner/TestJURunnerTest.java
+++ src/test/java/Parent/Runner/TestJURunnerTest.java
@@ -4,14 +4,18 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"Parent"},
+        snippets = CAMELCASE,
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html",
                 "json:target/cucumber-reports/Cucumber.json"}
+//        tags = "@account"
 )
 
 public class TestJURunnerTest {
